67fc9a5856948c89e9fa540a18010b5439c90135
amin4554 <anfs4554@gmail.com>
2025-08-27 14:32:43 +0200

    with reminder homepage finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/landing/Hero";
 import Steps from "@/components/landing/Steps";
 import WhyUs from "@/components/landing/WhyUs";
+import ShareCta from "@/components/landing/ShareCta";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <WhyUs />
       <Steps />
+      <ShareCta />
     </>
   );
 }
