d9c565827308b687921f4e79f93f3fb688cf93b1
kasra <kasrarahmanian3@gmail.com>
2025-08-28 04:30:57 +0330

    add conversation

--- src/app/CommentDetail/page.tsx
+++ src/app/CommentDetail/page.tsx
@@ -1,9 +1,11 @@
 import CommentDetail from "@/components/comment/CommentDetaol";
+import Conversation from "@/components/comment/Conversation";
 
 export default function addComment() {
   return (
     <>
       <CommentDetail />
+      <Conversation />
     </>
   );
 }
